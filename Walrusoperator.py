5b2ca55d98cb847b4ede892190d78c64dc7a513c
Selva Lakshmi <53254307+SV-SelvaLakshmi@users.noreply.github.com>
2021-03-16 16:48:46 +0530

    Create Walrusoperator.py

---
+++ Walrusoperator.py
@@ -0,0 +1,10 @@
+print("Hello Want to learn about walrus operator ? Here it is \n")
+print("WALRUS OPERATOR := -assings value to the variable as a part of large expression.\nIt syntax is variable name := expr\n")
+a= "Hello";
+if(n:=len(a)):
+  print(f"Length is {n}")
+greetings = "Welcome"
+while((n:=len(greetings))>1):
+      print(n);
+      greetings = greetings[:-1] 
+print(greetings) 
